30f2305ea4c928f710335f537024fa0934a87e5f
Gopikrishna <C.gopikrishna111@gmail.com>
2025-06-04 11:17:10 +0530

    Add files via upload

---
+++ Program-5/outputs.txt.py
@@ -0,0 +1,15 @@
+>>> 
+================== RESTART: D:\GKMCA 2\python\program-5\5.1.py =================
+brand: toyota, speed: 180 km/h
+>>> 
+================== RESTART: D:\GKMCA 2\python\program-5\5.2.py =================
+name:tillu,marks:92
+>>> 
+================== RESTART: D:\GKMCA 2\python\program-5\5.3.py =================
+area of Circle: 78.5
+>>> 
+================== RESTART: D:\GKMCA 2\python\program-5\5.4.py =================
+Book title: python 101
+Book title: AI Basics
+Book title: Data Science
+>>> 
